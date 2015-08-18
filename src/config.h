447ebb37e6a29bc00e1ce18386f2c3f3da3f0060
Hiroyuki Tanaka <tanakahx@gmail.com>
2015-08-18 14:59:38 +0900

    Implement naive alarm function. Delete a tick member of TCB.

--- src/config.h
+++ src/config.h
@@ -6,5 +6,6 @@ void sub_task1(int ex);
 void sub_task2(int ex);
 void sub_task3(int ex);
 void main_task(int ex);
+void main_task_callback(void);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include "uros.h"
 #include "uart.h"
 
-int id[4];
+int id[5];
+alarm_t alarm;
 
 void sub_task0(int ex)
 {
@@ -24,7 +25,7 @@ void sub_task1(int ex)
                 get_resource(0);
                 puts("[sub_task1]: set_event");
                 release_resource(0);
-                set_event(id[3], 0x1 << 0);
+                set_event(id[4], 0x1 << 0);
             }
         }
     }
@@ -45,7 +46,7 @@ void sub_task2(int ex)
                 get_resource(0);
                 puts("[sub_task2]: set_event");
                 release_resource(0);
-                set_event(id[3], 0x1 << 1);
+                set_event(id[4], 0x1 << 1);
             }
         }
     }
@@ -57,36 +58,54 @@ void sub_task3(int ex)
 
     while (1) {
         wait_event(0x3);
-        get_event(id[3], &ev);
+        get_event(id[4], &ev);
         if (ev == (0x1 << 0))
             puts("[sub_task3]: wake up by sub_task1");
         else if (ev == (0x1 << 1))
             puts("[sub_task3]: wake up by sub_task2");
         else
             puts("[sub_task3]: wake up by unknown");
-        clear_event(id[3], -1);
+        clear_event(id[4], -1);
     }
+}
+
+int called = 0;
+
+void main_task_callback(void)
+{
+    called = 1;
 }
 
 void main_task(int ex)
 {
+    tick_t t;
     puts("[main_task]: start");
+    get_alarm(0, &t);
+    printf("[main_task] alarm 0 is %d tick\n", t);
 
-    /* Create some tasks */
-    id[0] = declare_task(sub_task0, 0, 64);
-    id[1] = declare_task(sub_task1, 2, 256);
-    id[2] = declare_task(sub_task2, 2, 256);
-    id[3] = declare_task(sub_task3, 1, 256);
+    puts("[main_task]: waiting alarm event");
+    wait_event(0x1);
+    get_alarm(0, &t);
+    printf("[main_task] alarm 0 is %d tick\n", t);
+
+    puts("[main_task]: waiting alarm callback");
+    while (!called) ;
+    get_alarm(0, &t);
+    printf("[main_task] alarm 0 is %d tick\n", t);
+
+    cancel_alarm(0);
+    cancel_alarm(1);
+    cancel_alarm(2);
 
     /* Create resources */
     res[0].pri = 0;
 
     /* Start them */
-    activate_task(id[0]);
-    activate_task(id[0]); /* start again */
     activate_task(id[1]);
+    activate_task(id[1]); /* start again */
     activate_task(id[2]);
     activate_task(id[3]);
+    activate_task(id[4]);
 
     puts("[main_task]: done");
     terminate_task();
